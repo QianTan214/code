222e8659f1bcf5304e66b6f030ef20a78a26fcd5
QianTan214 <864709096@qq.com>
2021-04-05 22:28:39 +0800

    20210404

--- cs50x/finance/application.py
+++ cs50x/finance/application.py
@@ -47,79 +47,102 @@ if not os.environ.get("API_KEY"):
 def index():
     """Show portfolio of stocks"""
 
-    # get username
-    username = db.execute("select username from users where id = :user_id", user_id = session["user_id"])[0]["username"]
+    # check username of the user currently logged in
+    username = db.execute("select username from users where id = ?", session["user_id"])[0]["username"]
 
-    # get user's portfolio
-    rows = db.execute("select * from portfolio where username = :username", username = username)
+    # check what stocks the user owns
+    user_portfolio = db.execute("select symbol, shares from portfolio where username = ? order by symbol desc", username)
 
-    total_stock_value = 0
+    # check if user has portfolio, pass in parameter message
+    if not user_portfolio:
+        return render_template("index.html", message="Your portfolio is empty.")
 
-    for row in rows:
-        symbol = row["symbol"]
-        shares = row["shares"]
-        shares = int(shares)
-        price = lookup(symbol)["price"]
-        each_stock_value = shares * price
-        total_stock_value = total_stock_value + each_stock_value
+    total = 0
 
-    # get user's cash balance from users database
-    cash_balance = db.execute("select cash from users where username = :username", username = username])[0]["cash"]
+    # loop through user_portfolio
+    for portfolio in user_portfolio:
+        symbol = portfolio["symbol"]
+        stock_company = lookup(symbol)["name"]
+        shares = portfolio["shares"]
+        current_price = lookup(symbol)["price"]
+        total_value = shares * current_price
+        total = total + total_value
 
-    # user's grand total
-    total_money = cash_balance + total_stock_value
+    cash_balance = db.execute("select cash from users where id = ?", session["user_id"])[0]["cash"]
 
-    return render_template("index.html", symbol = symbol, shares = shares, each_stock_value = each_stock_value, total_stock_value = total_stock_value)
+    return render_template("index.html", username = username, user_portfolio = user_portfolio, cash_balance = cash_balance, total = total)
 
 
 @app.route("/buy", methods=["GET", "POST"])
 @login_required
 def buy():
     """Buy shares of stock"""
+
     if request.method == "POST":
         input_symbol = request.form.get("symbol")
         input_shares = request.form.get("shares")
 
         if not input_symbol or not lookup(input_symbol):
-            return apology("stock not found", 403)
+            return apology("stock not found", 400)
 
-        if not input_shares or not input_shares.isdigit() or int(input_shares) <= 0:
-            return apology("number of shares must be a positive integer", 403)
+        if not input_shares or int(input_shares) <= 0:
+            return apology("number of shares must be a positive integer", 400)
 
-        price = lookup(symbol)["price"]
-        total_price = price * int(shares)
+        stock = lookup(input_symbol)
 
-        rows = db.execute("select * from users where id = ?", session["user_id"]) # session["user_id"] is integer
-        cash_balance = rows[0]["cash"]
+        # get current price of a stock, not stock[0]["price"]
+        current_price = stock["price"]
 
-        # if user can't afford
-        if total_price > cash_balance:
-            return apology("insufficient cash to buy the number of shares entered", 403)
+        # caculate total price needed
+        total_price = current_price * int(input_shares)
 
-        username = db.execute("select username from users where id = :user_id", user_id = session["user_id"])[0]["username"]
+        # check user's cash balance in users table
+        cash_balance = db.execute("select cash from users where id = ?", session["user_id"])[0]["cash"]
 
-        # if user can afford, add transaction into history database
-        db.execute("insert into history (username, symbol, price, shares, operation) values (:username, :symbol, :price, :shares, "BUY")",
-            username = username,
-            symbol = symbol,
-            price = price,
-            shares = shares)
+        # error message if user can't afford
+        if cash_balance < total_price:
+            return apology("you don't have enough money to buy the shares", 403)
 
-        # add stock to user's portfolio database
-        db.execute("insert into portfolio (username, symbol, shares) values (:username, :symbol, :shares)",
-            username = username,
-            symbol = input_symbol,
-            shares = int(input_shares))
+        # new user's cash balance after buying
+        new_cash_balance = cash_balance - total_price
 
-        cash_balance = cash_balance - total_price
+        # insert transaction into history table
+        username = db.execute("select * from users where id = ?", session["user_id"])[0]["username"]
 
-        # update user's cash balance in users database
-        db.execute("update users set cash = :cash where id = :user_id", cash = cash_balance, user_id = session[user_id])
+        db.execute("insert into history (username, symbol, price, shares, operation) values (?, ?, ?, ?, ?)",
+            username, input_symbol, current_price, input_shares, "BUY")
 
+        # update user's cash balance in users table
+        db.execute("update users set cash = ? where id = ?", new_cash_balance, session["user_id"])
+
+        # check if user has shares of a particular stock before buying
+        # no [0]["shares"] follows after, will cause index out of range
+        shares_owned_before = db.execute("select shares from portfolio where username = ? and symbol = ?", username, input_symbol)
+
+        # if user doesn't have this particular stock before buying, insert into portfolio table
+        if not shares_owned_before:
+            db.execute("insert into portfolio (symbol, shares, username) values (?, ?, ?)", input_symbol, input_shares, username)
+
+        # if user already has this stock before buying
+        else:
+            shares_owned_after = shares_owned_before[0]["shares"] + int(input_shares)
+
+            # update user's portfolio in portfolio table
+            db.execute("update portfolio set shares = ? where symbol = ? and username = ?",
+                shares_owned_after, input_symbol, username)
+
+        # get company name
+        company_name = lookup(input_symbol)["name"]
+
+        # flash user with successfully buy message
+        # flash is like print in python, use {}
+        flash(f"Bought {input_shares} share(s) of {company_name} ({input_symbol}) successfully!")
+
+        # redirect user to homepage
         return redirect("/")
 
     else:
-        return render_template("/buy.html")
+        return render_template("buy.html")
 
 
 @app.route("/history")
@@ -127,8 +150,14 @@ def buy():
 def history():
     """Show history of transactions"""
 
-    stock_bought =
-    return apology("TODO")
+    # get username of the user currently logged in
+    username = db.execute("select username from users where id = ?", session["user_id"])[0]["username"]
+
+    # get all the transactions in history table, "transactions" is a list of dictionaries
+    transactions = db.execute("select * from history where username = ?", username)
+
+    # pass transactions into history.html page
+    return render_template("history.html", transactions = transactions)
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -183,54 +212,57 @@ def logout():
 def quote():
     """Get stock quote."""
 
+    # "POST" has to be capital letters
     if request.method == "POST":
         input_symbol = request.form.get("symbol")
 
         if not input_symbol or not lookup(input_symbol):
-            return apology("stock not found", 403)
+            return apology("stock not found", 400)
 
         stock = lookup(input_symbol)
 
-        return render_template("/quote.html", stock = stock)
+        return render_template("quote.html", stock = stock)
 
     else:
-        return render_template("/quote.html", stock = "")
-
+        return render_template("quote.html", stock ="")
 
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
+
+    # if method of request is "POST"
     if request.method == "POST":
         input_username = request.form.get("username")
         input_password = request.form.get("password")
-        confirmation = request.form.get("confirmation")
+        input_confirmation = request.form.get("confirmation")
 
         # check username
         if not input_username:
-            return apology("must provide username", 403)
+            return apology("must provide username", 400)
 
-        rows = db.execute("select * from user where username = ?", input_username)
+        rows = db.execute("select * from users where username = ?", input_username)
 
         if len(rows) != 0:
-            return apology("username already exists", 403)
+            return apology("username already exists", 400)
 
         # check password
         if not input_password:
-            return apology("must provide password", 403)
+            return apology("must provide password", 400)
 
         # double check password
-        if input_password != confirmation:
-            return apology("passwords do not match, please re-enter", 403)
+        if input_password != input_confirmation:
+            return apology("passwords do not match, please re-enter", 400)
 
-        # add a new user to users database
+        # add a new user to users table
         db.execute("insert into users (username, hash) values (?, ?)", input_username, generate_password_hash(input_password))
 
         # return to login page
-        return render_template("/login.html")
+        return render_template("login.html")
 
+    # if method of request is "GET"
     else:
-        return render_template("/register.html")
+        return render_template("register.html")
 
 
 @app.route("/sell", methods=["GET", "POST"])
@@ -238,66 +270,74 @@ def register():
 def sell():
     """Sell shares of stock"""
 
+    # get username of the user currently logged in
+    username = db.execute("select username from users where id = ?", session["user_id"])[0]["username"]
+
+    # check what stock symbols user has
+    symbols = db.execute("select symbol from portfolio where username = ?", username)
+
+    # if method of request is "POST"
     if request.method == "POST":
         input_symbol = request.form.get("symbol")
         input_shares = request.form.get("shares")
 
         if not input_symbol or not lookup(input_symbol):
-            return apology("stock not found", 403)
+            return apology("stock not found", 400)
 
-        if not input_shares or not input_shares.isdigit() or int(input_shares) <= 0:
-            return apology("number of shares must be a positive integer", 403)
+        if not input_shares or int(input_shares) <= 0:
+            return apology("number of shares must be a positive integer", 400)
 
-        # get username
-        username = db.execute("select username from users where id = :user_id",
-            user_id = session["user_id"])[0]["username"]
+        # check user's shares of a stock
+        shares_owned_before = db.execute("select shares from portfolio where username = ? and symbol = ?", username, input_symbol)
 
-        # check shares of a stock in portfolio database
-        total_shares = db.execute("select shares from portfolio where username = :username and symbol = :symbol",
-            username = username,
-            symbol = symbol)
+        # check whether user owns this stock or has enough shares of this stock
+        if not shares_owned_before or shares_owned_before[0]["shares"] < int(input_shares):
+            return apology("you don't have enough shares to sell", 403)
 
-        # check if user has the stock and if user sells more than he owns
-        if not total_stocks or total_stocks[0]["shares"] < input_shares:
-            return apology("insufficient shares to sell", 403)
+        # shares owned after selling
+        shares_owned_after = shares_owned_before[0]["shares"] - int(input_shares)
 
-        # add selling transaction in history database
-        db.execute("insert into history (username, symbol, price, shares, operation) values (:username, :symbol, :price, :shares, "SELL")",
-            username = username,
-            symbol = input_symbol,
-            price = lookup(symbol)["price"],
-            shares = int(input_shares))
+        # if user has no shares left after selling, delete it in user's portfolio
+        if shares_owned_after == 0:
+            db.execute("delete from portfolio where symbol = ? and username = ?", input_symbol, username)
 
-        # money earned by selling a stock
-        money_earned = int(input_shares) * lookup(symbol)["price"]
+        # if user has shares left after selling, update user's portfolio
+        db.execute("update portfolio set shares = ? where username = ? and symbol = ?", shares_owned_after, username, input_symbol)
 
-        # check user's current cash balance
-        cash_balance = db.execute("select cash from users where id = :user_id", user_id = session["user_id"])[0]["cash"]
+        # check current price of the stock
+        current_price = lookup(input_symbol)["price"]
 
-        # user's cash balance after selling a stock
-        cash_balance = cash_balance + money_earned
+        # money earned after selling
+        money_earned = current_price * int(input_shares)
 
-        # update user's cash in users database
-        db.execute("update users set cash = :cash where id = :user_id",
-            cash = cash_balance,
-            user_id = session["user_id"])
+        # check user's cash balance
+        cash_balance = db.execute("select cash from users where id = ?", session["user_id"])[0]["cash"]
 
-        # user's new shares of a stock after selling
-        new_shares = total_shares[0]["shares"] - int(input_shares)
+        # user's new cash balance after selling
+        new_cash_balance = cash_balance + money_earned
 
-        # update user's portfolio in portfolio database
-        db.execute("update portfolio set shares = :shares where username = :username",
-            shares = new_shares,
-            username = username)
+        # update user's cash balance after selling
+        db.execute("update users set cash = ? where id = ?", new_cash_balance, session["user_id"])
 
-        # flash user the selling alert
-        flash(f"Sold {input_shares} of shares of {input_symbol}")
+        # insert this transaction into history table
+        db.execute("insert into history (username, symbol, price, shares, operation) values (?, ?, ?, ?, ?)",
+            username, input_symbol, current_price, input_shares, "SELL")
+
+        # get company name
+        company_name = lookup(input_symbol)["name"]
+
+        # flash user with successfully sold message
+        # flash is like print in python, use {}
+        flash(f"Sold {input_shares} share(s) of {company_name} ({input_symbol}) successfully!")
 
         # redirect user to homepage
         return redirect("/")
 
+    # if method of request is "GET"
+    # symbols defined on top, it is a list of dictionaries
+    # pass symbols into sell.html page
     else:
-        return render_template("sell.html")
+        return render_template("sell.html", symbols = symbols)
 
 
 def errorhandler(e):
